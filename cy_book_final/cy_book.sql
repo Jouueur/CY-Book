51f1376b72f2d1b22478cd24d9cab5a4b6961db5
alexy <105677299+alakazar@users.noreply.github.com>
2024-05-27 00:03:18 +0200

    Add files via upload

---
+++ cy_book_final/cy_book.sql
@@ -0,0 +1,4 @@
+
+USE login_schema;
+SELECT * FROM login_schema.customer;
+INSERT INTO `login_schema`.`customer` ( `name_customer`, `last_name_customer`,`email_customer`) VALUES ( "zakaria", "leshob","vrai.dz.power@gmail.com");
